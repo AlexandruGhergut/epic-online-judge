fe9539ce6e43b5b57f41e1d35ef087146dda779f
alexandru <alexandru.ghergut94@gmail.com>
2017-04-16 09:41:57 +0300

    Polish navbar and login form

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field, Div, HTML, ButtonHolder
 
 
 class RegisterForm(UserCreationForm):
@@ -16,6 +19,40 @@ class RegisterForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username")
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('authentication:login')
+        self.helper.layout = Layout(
+            Div(
+                Field(
+                    'username',
+                    css_class='form-control',
+                    ),
+                css_class='form-group'
+                ),
+            Div(
+                HTML('<a class="pull-right" href="#">Forgot password?</a>'),
+                Field(
+                    'password',
+                    css_class='form-control'
+                    ),
+                css_class='form-group'
+            ),
+            Div(
+                HTML('<a href="{% url \'authentication:register\' %}">Don\'t have an account? Create one</a>'),
+                css_class='form-group'
+                ),
+            Div(
+                ButtonHolder(
+                    Submit('submit', 'Submit'),
+                ),
+                css_class='form-group',
+                ),
+        )
 
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get("username")
