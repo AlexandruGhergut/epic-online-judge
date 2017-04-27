a441adbb930ae9d97c258af2a3ed3b35632c1c77
alexandru <alexandru.ghergut94@gmail.com>
2017-04-27 21:58:59 +0300

    Fix access to user first and last name

--- authentication/models.py
+++ authentication/models.py
@@ -56,12 +56,12 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         Returns the first_name plus the last_name, with a space in between.
         """
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = '%s %s' % (self.profile.first_name, self.profile.last_name)
         return full_name.strip()
 
     def get_short_name(self):
         "Returns the short name for the user."
-        return self.first_name
+        return self.profile.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         """
