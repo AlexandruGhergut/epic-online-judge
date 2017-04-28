335378c87c73e78324ded61d882ed7107223dfda
alexandru <alexandru.ghergut94@gmail.com>
2017-04-28 20:48:03 +0300

    List problems and create common template

--- problemset/urls.py
+++ problemset/urls.py
@@ -6,4 +6,5 @@ app_name = 'problemset'
 urlpatterns = [
     url(r'^problem/create/$', views.CreateProblemView.as_view(),
         name='create_problem'),
+    url(r'^$', views.ListProblemsView.as_view(), name='list_problems'),
 ]
--- problemset/views.py
+++ problemset/views.py
@@ -1,13 +1,21 @@
+from datetime import datetime
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from problemset.forms import ProblemForm
+from .forms import ProblemForm
+from .models import Problem
 
 
 class CreateProblemView(LoginRequiredMixin, CreateView):
     form_class = ProblemForm
-    template_name = "problemset/create_problem.html"
+    template_name = "common/content_center_form.html"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(CreateProblemView, self).form_valid(form)
+
+
+class ListProblemsView(ListView):
+    template_name = 'problemset/list_problems.html'
+    queryset = Problem.objects.filter(publish_datetime__lte=datetime.now())
