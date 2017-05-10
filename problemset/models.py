6d4bf335c9cf1fef5f6ac900ffcd5ce075017070
alexandru <alexandru.ghergut94@gmail.com>
2017-05-10 15:45:16 +0300

    Add models for submission

--- problemset/models.py
+++ problemset/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from ckeditor.fields import RichTextField
-from common.storage import AzureStorage
 
 
 def testcase_directory_path(instance, filename):
     return 'problem/{0}/tests/{1}'.format(instance.problem.pk, filename)
+
+
+def source_directory_path(instance, filename):
+    return 'user/{0}/problem/{1}/{2}'.format(instance.user.pk,
+                                             instance.problem.pk, filename)
 
 
 class Problem(models.Model):
@@ -21,3 +25,22 @@ class TestCase(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     input_file = models.FileField(upload_to=testcase_directory_path)
     output_file = models.FileField(upload_to=testcase_directory_path)
+
+
+class SubmissionError(models.Model):
+    description = models.IntegerField()
+
+
+class Submission(models.Model):
+    STATUS_CHOICES = (
+        (0, 'Pending'),
+        (1, 'Success'),
+        (2, 'Error'),
+    )
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    source_file = models.FileField(upload_to=source_directory_path)
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    error = models.OneToOneField(SubmissionError, null=True)
+    score = models.IntegerField()
