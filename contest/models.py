d3c8b159ec5729f804e627c94cd9a589f93d6e12
alexandru <alexandru.ghergut94@gmail.com>
2017-06-22 23:01:35 +0300

    Add contest app

---
+++ contest/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+
+class Contest(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=64, unique=True)
+    publish_datetime = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
